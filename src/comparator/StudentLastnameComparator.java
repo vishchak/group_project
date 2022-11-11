19a145651f53473839b74483ac0e8acdf808ddae
vishchak <denis.vishchak@gmail.com>
2022-11-11 12:36:50 +1100

    Generify StudentLastnameComparator;

--- src/comparator/StudentLastnameComparator.java
+++ src/comparator/StudentLastnameComparator.java
@@ -4,14 +4,12 @@ import human_beings.Student;
 
 import java.util.Comparator;
 
-public class StudentLastnameComparator implements Comparator {
+public class StudentLastnameComparator implements Comparator<Student> {
     @Override
-    public int compare(Object o1, Object o2) {
-        Student student1 = (Student) o1;
-        Student student2 = (Student) o2;
+    public int compare(Student o1, Student o2) {
 
-        String lastname1 = student1.getLastname();
-        String lastname2 = student2.getLastname();
+        String lastname1 = o1.getLastname();
+        String lastname2 = o2.getLastname();
 
         if (lastname1.compareTo(lastname2) > 0) {
             return 1;
